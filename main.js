46e0aaa34b31ad6bfd7b8a8cdf1dc4382b4201eb
leeejin <asd72621@naver.com>
2024-04-29 16:16:28 +0900

    개인과제 - 영화정보검색사이트

--- main.js
+++ main.js
@@ -1,85 +1,89 @@
 const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDIzYzU3MWQ5M2FjZTdiYWQ3YTFkZWE3NWI0YzhhYiIsInN1YiI6IjY2MjY0NjQ0Y2I2ZGI1MDE2M2FlZTI3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FfCrs2-T_js5Cuud3FBlI-obOWez3bf5_bj5KGmuVC0'
-    }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDIzYzU3MWQ5M2FjZTdiYWQ3YTFkZWE3NWI0YzhhYiIsInN1YiI6IjY2MjY0NjQ0Y2I2ZGI1MDE2M2FlZTI3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FfCrs2-T_js5Cuud3FBlI-obOWez3bf5_bj5KGmuVC0",
+  },
 };
+const $content = document.getElementById("eiga");
+const $input = document.getElementById("searchText");
 let movies = [];
+const apiUrl =
+"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
-const apiUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
 fetch(apiUrl, options)
-    .then(response => response.json())
-    .then(response => {
-        movies = response.results;
-        displayMovieData(movies);
-        console.log(response);
-    })
-    .catch(err => console.error(err));
+  .then((response) => response.json())
+  .then((response) => {
+    movies = response.results;
+    displayMovieData(movies);
+    console.log(response);
+  })
+  .catch((err) => console.error(err));
 
 function displayMovieData(m) {
-    let total = "☆☆☆☆☆";
-    const card = document.getElementById('eiga');
-    m.forEach((movie) => {
-        const cardDiv = document.createElement('div');
-        cardDiv.onclick = () => handleAlert(movie.id);
-        cardDiv.classList.add('card');
+  let total = "☆☆☆☆☆";
+  m.forEach((movie) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.onclick = () => handleAlert(movie.id);
+    cardDiv.classList.add("card");
 
-        const img = document.createElement('img');
-        img.id = 'img';
-        img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const img = document.createElement("img");
+    img.id = "img";
+    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-        const overviewDiv = document.createElement('div');
-        overviewDiv.id = 'overviewDiv';
+    const overviewDiv = document.createElement("div");
+    overviewDiv.id = "overviewDiv";
 
-        const overview = document.createElement('p');
-        overview.id = 'overview';
-        overview.textContent = movie.overview;
-        overviewDiv.append(overview);
+    const overview = document.createElement("p");
+    overview.id = "overview";
+    overview.textContent = movie.overview;
+    overviewDiv.appendChild(overview);
 
-        const vote = document.createElement('p');
-        vote.id = 'vote';
+    const vote = document.createElement("p");
+    vote.id = "vote";
 
-        let count = (movie.vote_average / 2).toFixed(1);
-        total = "⭐".repeat(parseInt(count)) + total.slice(parseInt(count));
-        vote.textContent = `${total} (${count})`;
+    let count = (movie.vote_average / 2).toFixed(1);
+    total = "⭐".repeat(parseInt(count)) + total.slice(parseInt(count));
+    vote.textContent = `${total} (${count})`;
 
-        const title = document.createElement('h3');
-        title.id = 'title';
-        title.textContent = movie.title;
+    const title = document.createElement("h3");
+    title.id = "title";
+    title.textContent = movie.title;
 
-        const language = document.createElement('p');
-        language.id = 'language';
-        language.textContent = 'language : ' + movie.original_language;
+    const language = document.createElement("p");
+    language.id = "language";
+    language.textContent = "language : " + movie.original_language;
 
-        cardDiv.appendChild(img);
-        cardDiv.appendChild(overviewDiv);
-        cardDiv.appendChild(title);
-        cardDiv.appendChild(vote);
-        cardDiv.appendChild(language);
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(overviewDiv);
+    cardDiv.appendChild(title);
+    cardDiv.appendChild(vote);
+    cardDiv.appendChild(language);
 
-        cardDiv.addEventListener('mouseover', () => {
-            overviewDiv.style.opacity = 1;
-        });
-        cardDiv.addEventListener('mouseout', () => {
-            overviewDiv.style.opacity = 0;
-        });
-        card.appendChild(cardDiv);
-
-    })
+    cardDiv.addEventListener("mouseover", () => {
+      overviewDiv.style.opacity = 1;
+    });
+    cardDiv.addEventListener("mouseout", () => {
+      overviewDiv.style.opacity = 0;
+    });
+    $content.appendChild(cardDiv);
+  });
 }
 
 function handleAlert(id) {
-    alert("영화 id:" + id);
+  alert("영화 id:" + id);
 }
 
 function handleSearch() {
-    const searchText = document.getElementById('searchText').value.toLowerCase();
-    const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchText));
+  const searchText = $input.value.toLowerCase();
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchText)
+  );
 
-    document.getElementById('eiga').innerHTML = '';
-    displayMovieData(filteredMovies);
-};
+  $content.innerHTML = "";
+  displayMovieData(filteredMovies);
+}
 function handleLocation() {
-    window.location.href = "main.html";
+  window.location.href = "/index.html";
 }
